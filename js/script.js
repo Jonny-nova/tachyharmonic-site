1cdb2d8dc4600f18fefc390d1a49699f69c1bba3
Jonny-nova <tachyharmonicintelligence@gmail.com>
2025-06-05 08:56:57 +0100

    Add interactive egg script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+'use strict';
 
+(function() {
+  const egg = document.querySelector('.egg');
+  const response = document.querySelector('.response-text');
+
+  if (!egg || !response) return;
+
+  const updateMessage = () => {
+    response.textContent = 'The egg is hatching\u2026';
+    response.classList.remove('text-transition');
+    // force reflow to restart animation if triggered multiple times
+    void response.offsetWidth;
+    response.classList.add('text-transition');
+  };
+
+  egg.addEventListener('click', updateMessage);
+  egg.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' || e.key === ' ' || e.key === 'Spacebar') {
+      updateMessage();
+    }
+  });
+})();
